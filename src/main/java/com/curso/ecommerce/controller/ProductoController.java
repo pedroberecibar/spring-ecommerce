a595fe0c2825c8fe9eec247e9c062b31d544f563
pedroberecibar <pedro.berecibar23@gmail.com>
2023-10-18 12:52:00 -0300

    Funcionalidad para eliminar un registro Producto

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -7,10 +7,7 @@ import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -71,6 +68,12 @@ public class ProductoController {
     public String update(Producto producto){
         productoService.update(producto);
         return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        productoService.delete(id);
+        return"redirect:/productos";
     }
 
 }
